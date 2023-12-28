657f1e4d3fba906205eb5ee776b2f12072daaff7
Ivan Shakin <ivanxshakin1993@gmail.com>
2023-12-28 23:24:45 +0300

    Control file finished

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел:");
+string[] input = Console.ReadLine().Split(' ');
+
+Console.WriteLine("Первоначальный массив:");
+for (int i = 0; i < input.Length; i++)
+{
+    Console.Write(input[i] + " ");
+}
+
+Console.WriteLine();
+
+int count = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+if (count > 0)
+{
+    string[] output = new string[count];
+    int index = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            output[index] = input[i];
+            index++;
+        }
+    }
+
+    Console.WriteLine("Новый массив:");
+    for (int i = 0; i < output.Length; i++)
+    {
+        Console.Write(output[i] + ' ');
+    }
+}
+
+else
+{
+    Console.WriteLine("Массив не получился. Все элементы содержат больше 3-х символов.");
+}
